185c3e452b3bd346fc72579c7b6f815621adc2e5
Tsuyoshi-Ishikawa <tsuyozo7@gmail.com>
2022-09-27 10:14:41 +0900

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+  });
   await app.listen(3000);
 }
 bootstrap();
