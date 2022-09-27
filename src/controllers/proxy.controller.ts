3898cec3e86f1c3df89ab7b704de416153788802
Tsuyoshi-Ishikawa <tsuyozo7@gmail.com>
2022-09-27 16:16:00 +0900

    set body validator

--- src/controllers/proxy.controller.ts
+++ src/controllers/proxy.controller.ts
@@ -1,14 +1,27 @@
-import { Controller, Post, Req } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AppService } from '../services/app.service';
+import { IsString, IsInt, IsArray } from 'class-validator';
+
+class VerseRequestDto {
+  @IsString({ message: 'invalid JSON-RPC version' })
+  jsonrpc: string;
+
+  @IsInt({ message: 'invalid ID' })
+  id: number;
+
+  @IsString({ message: 'rpc method is not whitelisted' })
+  method: string;
+
+  @IsArray({ message: 'expected params array of at least 1 argument' })
+  params: [];
+}
 
 @Controller()
 export class ProxyController {
   constructor(private readonly appService: AppService) {}
 
   @Post()
-  redirectVerse(@Req() request: Request) {
-    console.log('===========');
-    console.log(request.body);
+  redirectVerse(@Body() verseRequestDto: VerseRequestDto) {
     return {};
   }
 }
