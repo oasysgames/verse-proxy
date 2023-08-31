2975423e6d86f2a5fed941f4ea82f74852886f02
Tsuyoshi-Ishikawa <tsuyozo7@gmail.com>
2023-08-31 15:18:54 +0900

    set heartbeat on application bootstrap

--- src/jobs/heartbeat.service.ts
+++ src/jobs/heartbeat.service.ts
@@ -1,14 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import { Interval } from '@nestjs/schedule';
 import { HeartbeatMillisecondInterval } from 'src/constants';
 import { DatastoreService } from 'src/datastore/services';
 
 @Injectable()
-export class HeartbeatService {
+export class HeartbeatService implements OnApplicationBootstrap {
   constructor(private datastoreService: DatastoreService) {}
 
   @Interval(HeartbeatMillisecondInterval)
   handleInterval() {
+    console.log('Called setHeartBeat');
+    this.datastoreService.setHeartBeat();
+  }
+
+  onApplicationBootstrap() {
     console.log('Called setHeartBeat');
     this.datastoreService.setHeartBeat();
   }
